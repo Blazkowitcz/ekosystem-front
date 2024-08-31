beb5a0d24700be2eb3ddfa9b04d2a7fa7f6e4881
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-08-31 21:25:21 +0200

    Add react-bootstrap and navbar to main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
-import  "bootstrap/dist/css/bootstrap.min.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }

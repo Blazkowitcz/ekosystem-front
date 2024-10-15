7cd41de4024a48b7e3454b45ba0e43625c347501
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-10-15 17:15:48 +0200

    Update dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,76 +1,120 @@
 'use client'
 import { createColumnHelper } from '@tanstack/react-table'
-import { DashboardCard } from '@/app/components/Dashboard'
+import { Badge } from 'react-bootstrap'
+import { DashboardCard, DashboardDiskCard } from '@/app/components/Dashboard'
+import { Table } from '@/app/components/Table'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import { useFetch } from '@/utils/hooks/useFetch'
+import { useState } from 'react';
 
 export default function RegisterPage() {
 
+  const [detail, setDetail] = useState(null)
+  const { data, isLoading } = useFetch(`http://${process.env.NEXT_PUBLIC_BACKEND_ADDRESS}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/systems`);
+
   ChartJS.register(ArcElement, Tooltip, Legend);
 
-  const data = [
-    { id: '1', name: 'PC 1', OSDescription: 'Nobara - Fedora 40', OSType: 'linux' },
-    { id: '2', name: 'PC 2', OSDescription: 'Windows 10 - 64x', OSType: 'windows' },
-    { id: '3', name: 'NAS Synology', OSDescription: 'Synology', OSType: 'linux' }
-  ]
-
-  let chartData = [
+  const columns = [
     {
-      label: "Free",
-      value: 34,
-      color: "rgba(0, 43, 73, 1)",
-      cutout: "50%",
+      id: 'name',
+      header: 'Name',
+      accessorKey: 'name',
     },
     {
-      label: "Used",
-      value: 66,
-      color: "rgba(0, 103, 160, 1)",
-      cutout: "50%",
+      id: 'osDescription',
+      header: 'OS Description',
+      accessorKey: 'description',
+    },
+    {
+      id: 'osType',
+      header: 'OS Type',
+      accessorKey: 'os',
+    },
+    {
+      id: 'statut',
+      header: 'Status',
+      cell: ({ row }) => {
+        return row.original.alive ? <Badge bg="success">Connected</Badge> : <Badge bg="danger">Disconnected</Badge>
+      }
     }
-  ]
+  ];
 
-  const finalData = {
-    labels: chartData.map((item) => item.label),
-    datasets: [
-      {
-        data: chartData.map((item) => Math.round(item.value)),
-        backgroundColor: chartData.map((item) => item.color),
-        borderColor: chartData.map((item) => item.color),
-        borderWidth: 1,
-        dataVisibility: new Array(chartData.length).fill(true),
-      },
-    ],
+  /**
+   * Fetch System detail on row click
+   * @param {Object} row 
+   */
+  const handleRowClick = async (row) => {
+    const data = await fetch(`http://${process.env.NEXT_PUBLIC_BACKEND_ADDRESS}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/systems/${row.original.id}/details`);
+    const result = await data.json();
+    setDetail(result)
   };
-
-  const options = {
-    plugins: {
-      responsive: true,
-    },
-    cutout: chartData.map((item) => item.cutout),
-  };
-
 
   return (
-    <>
-      <div className='m-auto mt-4' style={{ width: '90%' }}>
-        {data.map(d => {
-          return (
-            <>
-              <DashboardCard key={d.id} name={d.name} OSDescription={d.OSDescription} OSType={d.OSType} onClick={() => console.log(d.name)} />
-            </>
-          )
-        })}
-        <div className='row mt-3'>
-          <div className='bg-white me-3' style={{ width: '15%' }}>
-            <span className='text-center d-block'>Espace Disque</span>
-            <Doughnut data={finalData} options={options} />
+    <div className='m-3'>
+      {!isLoading && (<Table data={data} columns={columns} handleRowClick={handleRowClick} />)}
+      <hr className="hr hr-blurry mt-4" />
+      {detail && (
+        <>
+          <div className='bg-secondary mt-2 text-center text-light pt-2 pb-2'>{detail.name}</div>
+          <div className='d-flex'>
+            <div className='bg-secondary mt-2 w-50 me-2'>
+              <div>
+                <span className='text-center d-block text-light mb-3'>Details</span>
+                <div className='d-flex pb-3'>
+                  <div className='container'>
+                    <div className='row'>
+                      <div className='col-6 text-center text-light '>Processeur : {detail.processor}</div>
+                      <div className='col-6 text-center text-light '>Coeurs : {detail.cores}</div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-6 text-center text-light '>Architecture : {detail.architecture}</div>
+                      <div className='col-6 text-center text-light '>hostname : {detail.hostname}</div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-6 text-center text-light '>Plateforme : {detail.os}</div>
+                      <div className='col-6 text-center text-light '>Version : {detail.release}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='bg-secondary mt-2 w-50'>
+              <div>
+                <span className='text-center d-block text-light mb-3'>Network</span>
+                <div className='d-flex pb-3'>
+                  <div className='container'>
+                    {Object.keys(detail.networkInterfaces).map((key, index) => (
+                      <div className='row' key={index}>
+                        <div className='col-6 text-center text-light'>
+                          IP: {detail.networkInterfaces[key][0]?.address || 'N/A'}
+                        </div>
+                        <div className='col-6 text-center text-light'>
+                          Mask: {detail.networkInterfaces[key][0]?.netmask || 'N/A'}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
-          <div className='bg-white' style={{ width: '15%' }}>
-            <span className='text-center d-block'>Espace Disque</span>
-            <Doughnut data={finalData} options={options} />
-          </div>
-        </div>
-      </div>
-    </>
+
+          {detail.disks && (<div className='bg-secondary mt-2'>
+            <div>
+              <span className='text-center d-block text-light mb-3'>Disques</span>
+              <div className='d-flex justify-content-center pb-3'>
+                {detail.disks.map(disk => {
+                  return (
+                    <DashboardDiskCard key={disk.filesystem} name={disk.filesystem} data={disk} />
+                  )
+                })}
+              </div>
+            </div>
+          </div>)}
+        </>
+      )}
+    </div>
   );
 }

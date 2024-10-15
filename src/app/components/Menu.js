fce87ae7c15aa546b3723c9aa037760e31136984
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-10-15 17:13:37 +0200

    Update Menu component

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,15 +1,15 @@
 'use client'
 import { Nav, Navbar, Container } from "react-bootstrap"
 const Menu = () => {
-  return (<Navbar expand="lg" className="bg-white">
+  return (<Navbar expand="lg" className="bg-dark">
     <Container>
-      <Navbar.Brand href="">Ekosystem</Navbar.Brand>
+      <Navbar.Brand href="" className="text-light">{`[Ekosystem]`}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="torrents">Torrents</Nav.Link>
-          <Nav.Link href="explorer">File Explorer</Nav.Link>
+          <Nav.Link href="dashboard" className="text-light">Dashboard</Nav.Link>
+          <Nav.Link href="torrents" className="text-light">Torrents</Nav.Link>
+          <Nav.Link href="explorer" className="text-light">File Explorer</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>

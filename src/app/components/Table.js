ad2833da0dea6380b52b2e217efdd241318e1402
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-10-15 17:13:19 +0200

    Update Table component

--- src/app/components/Table.js
+++ src/app/components/Table.js
@@ -1,54 +1,60 @@
-
-import * as React from 'react'
-import { flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
-import { Table as BTable } from 'react-bootstrap'
-import '@/style/components/Table.css'
+import * as React from 'react';
+import { flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { Table as BTable } from 'react-bootstrap';
+import '@/style/components/Table.css';
 
 function Table(props) {
-    const {
-        data,
-        columns
-    } = props;
-  
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-    })
-  
-    return (
-        <div className="p-2">
-          <BTable striped responsive size="sm">
-            <thead>
-              {table.getHeaderGroups().map(headerGroup => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map(header => (
-                    <th key={header.id} colSpan={header.colSpan}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </thead>
-            <tbody>
-              {table.getRowModel().rows.map(row => (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map(cell => (
-                    <td key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </BTable>
-        </div>
-      )
-  }
+  const { 
+    data, 
+    columns,
+    handleRowClick = null
+  } = props;
 
-export default Table;
+  // Configuration de la table
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <BTable striped hover className=''>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {flexRender(header.column.columnDef.header, header.getContext())}
+                {header.column.getCanSort() ? (
+                  <span>
+                    {header.column.getIsSorted()
+                      ? header.column.getIsSortedDesc()
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                ) : null}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr 
+            key={row.id}
+            onClick={props.handleRowClick ? () => handleRowClick(row) : undefined}
+          >
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </BTable>
+  );
+}
+
+export { Table };
